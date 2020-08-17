2539360dffeec4ac85efb2c6ef7aac7354e94882
BrainStone <yannick@tedworld.de>
2020-08-17 23:03:05 +0200

    First class file and added the web server

---
+++ src/main/java/me/lucko/luckperms/extension/directeditor/DirectEditorExtension.java
@@ -0,0 +1,24 @@
+package me.lucko.luckperms.extension.directeditor;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.extension.Extension;
+
+public class DirectEditorExtension implements Extension {
+    private final LuckPerms luckPerms;
+
+    public DirectEditorExtension(LuckPerms luckPerms) {
+        this.luckPerms = luckPerms;
+    }
+
+    @Override
+    public void load() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void unload() {
+        // TODO Auto-generated method stub
+
+    }
+}
